1562f113ecccc8dc04cb0d6a5c69c44b08a7d181
Vinicius Pacheco <vfpweb@gmail.com>
2017-10-08 17:39:11 -0300

    Added sports service and politics service

---
+++ SportsNewsService/app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+from views import sports_news
+from models import db
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+db.init_app(app)
+
+# register blueprints
+app.register_blueprint(sports_news)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
